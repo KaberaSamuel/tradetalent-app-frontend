dd3a0b0684dcbb4b8a3d5a25ef3a1c5d6bcb9c85
KaberaSamuel <kaberanshutis@gmail.com>
2025-07-25 17:45:36 +0200

    basic styles for login and signup pages

--- my-react-ts-app/src/components/Login.tsx
+++ my-react-ts-app/src/components/Login.tsx
@@ -1,26 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChrome } from "@fortawesome/free-brands-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
+
   return (
-    <div>
-      <form>
-        <h1>Login In to TradeSkill</h1>
+    <div className="h-screen text-neutral-500 text-center flex justify-center items-center">
+      <form className="w-[600px] py-10 px-8 bg-neutral-50 flex flex-col gap-4 border border-neutral-200  rounded-2xl [&_p]:text-left [&_p]:text-black [&_p]:mb-2 [&_input]:w-full [&_input]:bg-neutral-100 [&_input]:border [&_input]:border-neutral-200 [&_input]:py-2.5 [&_input]:px-3 [&_input]:text-black [&_input]:placeholder-gray-500 [&_input]:rounded-xl">
+        <h1 className="text-black text-3xl font-semibold">
+          Login In to TradeTalent
+        </h1>
         <div>
-          <label>Email</label>
+          <p>Email</p>
           <input type="email" placeholder="Enter your email" />
         </div>
         <div>
-          <label>Password</label>
-          <input type="password" placeholder="Enter your password" />
-        </div>
-        <button>Login</button>
-        <button>Login with Google</button>
-
-        <div>
-          <a href="#">Forgot Password?</a>
-          <div>
-            <p>Don't have an account ?</p> <Link to="/signup">Sign Up</Link>
+          <p>Password</p>
+          <div className="relative">
+            <input
+              type={passwordVisibility ? "input" : "password"}
+              placeholder="Enter your password"
+            />
+            <FontAwesomeIcon
+              icon={passwordVisibility ? faEye : faEyeSlash}
+              onClick={() => {
+                setPasswordVisibility(!passwordVisibility);
+              }}
+              className="absolute right-1 top-1/2 transform -translate-1/2"
+            />
           </div>
+        </div>
+        <button className="p-2.5 bg-teal-500 text-white font-semibold rounded-2xl">
+          Log In
+        </button>
+        <button className="p-2.5 bg-white text-black rounded-2xl font-semibold flex justify-center items-center gap-1 border border-neutral-200">
+          <FontAwesomeIcon icon={faChrome} />
+          <span>Log In with Google</span>
+        </button>
+
+        <div className="text-teal-500 font-semibold">
+          <a href="#">Forgot Password?</a>
+        </div>
+
+        <div className="text-teal-500 font-semibold flex justify-center items-center gap-2">
+          <span>Don't have an account ?</span> <Link to="/signup">Sign Up</Link>
         </div>
       </form>
     </div>
--- my-react-ts-app/src/components/Signup.tsx
+++ my-react-ts-app/src/components/Signup.tsx
@@ -1,18 +1,63 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChrome } from "@fortawesome/free-brands-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Signup = () => {
+  const [passwordVisibility1, setPasswordVisibility1] = useState(false);
+  const [passwordVisibility2, setPasswordVisibility2] = useState(false);
+
   return (
-    <div>
-      <form>
-        <h1>Join Our Community</h1>
-        <p>Create your account to start trading skills</p>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
-        <button>Sign Up</button>
-        <button>Sign Up with Google</button>
-        <div>
-          <p>Already have an account ?</p> <Link to="/login">Log In</Link>
+    <div className="h-screen text-neutral-500 text-center flex justify-center items-center">
+      <form className="w-[600px] py-10 px-8 bg-neutral-50 flex flex-col gap-4 border border-neutral-200  rounded-2xl [&>*]:w-full [&_input]:w-full [&_input]:bg-neutral-100 [&_input]:border [&_input]:border-neutral-200 [&_input]:py-2.5 [&_input]:px-3 [&_input]:text-black [&_input]:placeholder-gray-500 [&_input]:rounded-xl">
+        <h1 className="text-black text-3xl font-semibold">
+          Join Our Community!
+        </h1>
+        <p>Create your account to start trading your talents.</p>
+        <input className="min-w-[220px]" type="email" placeholder="Email" />
+
+        <div className="relative">
+          <input
+            type={passwordVisibility1 ? "input" : "password"}
+            placeholder="Enter your password"
+          />
+          <FontAwesomeIcon
+            icon={passwordVisibility1 ? faEye : faEyeSlash}
+            onClick={() => {
+              setPasswordVisibility1(!passwordVisibility1);
+            }}
+            className="absolute right-1 top-1/2 transform -translate-1/2"
+          />
+        </div>
+
+        <div className="relative">
+          <input
+            type={passwordVisibility2 ? "input" : "password"}
+            placeholder="Confirm password"
+          />
+          <FontAwesomeIcon
+            icon={passwordVisibility2 ? faEye : faEyeSlash}
+            onClick={() => {
+              setPasswordVisibility2(!passwordVisibility2);
+            }}
+            className="absolute right-1 top-1/2 transform -translate-1/2"
+          />
+        </div>
+
+        <button className="p-2.5 bg-teal-500 text-white font-semibold rounded-2xl">
+          Sign Up
+        </button>
+        <button className="p-2.5 bg-white text-black rounded-2xl font-semibold flex justify-center items-center gap-1 border border-neutral-200">
+          <FontAwesomeIcon icon={faChrome} />
+          <p>Sign Up with Google</p>
+        </button>
+
+        <div className="mt-5 flex justify-center items-center gap-2">
+          <p>Already have an account ?</p>{" "}
+          <Link to="/login" className="text-teal-500 font-semibold">
+            Log In
+          </Link>
         </div>
       </form>
     </div>
