83b5b6bc41be18680fde9e36d2380fd905bf4562
KaberaSamuel <kaberanshutis@gmail.com>
2025-07-25 04:50:46 +0200

    basic styles for the welcome page

--- my-app/src/components/Welcome.tsx
+++ my-app/src/components/Welcome.tsx
@@ -2,16 +2,25 @@ import { Link } from "react-router-dom";
 
 const WelcomePage = () => {
   return (
-    <div>
-      <h1>Exchange Skils, Grow Together</h1>
-      <p>
-        Connect with a community of talented individuals to share your expertise
-        or find the skills you need. Learn, teach, and collaborate effortlessly
-      </p>
+    <div className="w-screen  flex justify-center">
+      <div className="w-1/2 h-screen text-center flex flex-col justify-center items-center gap-9">
+        <h1 className="text-5xl leading-tight font-semibold">
+          Exchange Skills, Grow Together
+        </h1>
+        <p className="text-lg text-neutral-500">
+          Connect with a community of talented individuals to share your
+          expertise or find the skills you need. Learn, teach, and collaborate
+          effortlessly
+        </p>
 
-      <div>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Login</Link>
+        <div className="flex gap-5 *:py-2.5 *:px-5 *:rounded-2xl *:font-semibold">
+          <Link to="/signup" className="bg-teal-500 text-white">
+            Sign Up
+          </Link>
+          <Link to="/login" className="bg-teal-100 text-teal-500">
+            Log In
+          </Link>
+        </div>
       </div>
     </div>
   );
